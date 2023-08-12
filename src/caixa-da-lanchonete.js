ac72b0aa9c04320608dc2926518268da0e98d373
Maycon Padilha <maycon.jordan5000@gmail.com>
2023-08-12 12:41:05 -0300

    Testes Completos

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -17,33 +17,61 @@ class CaixaDaLanchonete {
 
         let valorTotal = 0;
 
+        if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!";
+        }
+
         for (const item of itens) {
-            const [codigo, quantidade] = item.split(',');
+            const [codigo, quantidade] = item.split(",");
+
+            if(parseInt(quantidade) === 0){
+                return "Quantidade inválida!";
+            }
+
+            if(codigo === "chantily"){
+                const itemEncontrado = buscarItemNoCarrinho("cafe", itens);
+                if(itemEncontrado === null){
+                    return "Item extra não pode ser pedido sem o principal";
+                }
+            }
+
+            if(codigo === "queijo"){
+                const itemEncontrado = buscarItemNoCarrinho("sanduiche", itens);
+                if(itemEncontrado === null){
+                    return "Item extra não pode ser pedido sem o principal";
+                }
+            }
 
             if (cardapio[codigo]) {
                 valorTotal += cardapio[codigo] * parseInt(quantidade);
             } else {
-                return 'Item inválido!';
+                return "Item inválido!";
             }
         }
 
-        if(metodoDePagamento === null){
-            return "Não há itens no carrinho de compra!";
+        function buscarItemNoCarrinho(codigoBuscado, itens) {
+            for (const item of itens) {
+                const [codigo] = item.split(',');
+                if (codigo === codigoBuscado) {
+                    return item;
+                }
+            }
+            return null;
         }
 
-        if (metodoDePagamento === 'dinheiro') {
-            valorTotal *= (1 - descontoDinheiro); // Aplica desconto de 5% no valor 
-            return `R$ ${valorTotal.toFixed(2).replace('.', ',')}`;
-        } else if (metodoDePagamento === 'credito') {
-            valorTotal *= (1 + acrescimoCredito); // Aplica acréscimo de 3% no valor total
-            return `R$ ${valorTotal.toFixed(2).replace('.', ',')}`;
-        } else if (metodoDePagamento === 'debito') {
-            return `R$ ${valorTotal.toFixed(2).replace('.', ',')}`;
+        if (metodoDePagamento === "dinheiro") {
+            valorTotal *= (1 - descontoDinheiro); 
+            return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
+        } else if (metodoDePagamento === "credito") {
+            valorTotal *= (1 + acrescimoCredito);
+            return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
+        } else if (metodoDePagamento === "debito") {
+            return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
         } else {
             return "Forma de pagamento inválida!";
         }
 
-        return `R$ ${valorTotal.toFixed(2).replace('.', ',')}`;
+        // return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
     }
 }
 
